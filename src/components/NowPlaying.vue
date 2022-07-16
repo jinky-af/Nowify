1ab608dbf70047989c96d219b1ab1f50a844ee18
jinky-af <79592905+jinky-af@users.noreply.github.com>
2022-07-16 16:39:38 -0400

    Update NowPlaying.vue

--- src/components/NowPlaying.vue
+++ src/components/NowPlaying.vue
@@ -7,13 +7,7 @@
     >
       <div class="now-playing__cover">
         <img
-            width: 100%;
-            height: 100%;
-            position: fixed;
-            right: 0;
-            left: 0;
-            top: 0;
-            bottom: 0;
+·width:·100%;·height:·100%;
           :src="player.trackAlbum.image"
           :alt="player.trackTitle"
           class="now-playing__image"
