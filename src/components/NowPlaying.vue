a772b484a9555b0860570dae8ad86dbff1f0c879
jinky-af <79592905+jinky-af@users.noreply.github.com>
2022-07-16 16:52:28 -0400

    Update NowPlaying.vue

--- src/components/NowPlaying.vue
+++ src/components/NowPlaying.vue
@@ -7,12 +7,15 @@
     >
       <div class="now-playing__cover">
         <img
+          width: 100%;
+          height: auto;
+          background-size: cover;
+          background-position: center center;
           :src="player.trackAlbum.image"
           :alt="player.trackTitle"
           class="now-playing__image"
         />
-      </div>
-      
+      </div>  
     </div>
     <div v-else class="now-playing" :class="getNowPlayingClass()">
       <h1 class="now-playing__idle-heading">No music is playing 😔</h1>
